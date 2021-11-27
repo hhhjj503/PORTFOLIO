190c8fcf416380da6c09817a9c981bd08fb4c457
nav <hhj@naver.com>
2021-11-27 23:22:00 +0900

    20211127

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  /*
+  const tech = document.querySelector(".tech");
+  const t_title = document.querySelector(".t_title");
+
+  tech.addEventListener("mouseover", function () {
+    t_title.style.color = "black";
+  });
+
+  tech.addEventListener("mouseleave", function () {
+    t_title.style.color = "white";
+  });
+  */
+};
