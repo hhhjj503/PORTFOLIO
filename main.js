c05ce13c39ec3eec31ff1e1defb447e5cc1284e5
nav <hhj@naver.com>
2021-11-29 20:17:38 +0900

    20211129

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 window.onload = function () {
-  /*
-  const tech = document.querySelector(".tech");
-  const t_title = document.querySelector(".t_title");
+  const lis = document.querySelectorAll(".random");
+  for (let i = 0; lis.length; i++) {
+    lis[i].addEventListener("mouseover", function () {
+      changeColor(lis[i]);
+    });
+    lis[i].addEventListener("mouseleave", function () {
+      originalColor(lis[i]);
+    });
+  }
 
-  tech.addEventListener("mouseover", function () {
-    t_title.style.color = "black";
-  });
+  function changeColor(li) {
+    const colorValue = makingcolor();
+    li.style.color = colorValue;
+    if (colorValue === "white") {
+      li.style.border = "1px solid #000000";
+    }
+  }
 
-  tech.addEventListener("mouseleave", function () {
-    t_title.style.color = "white";
-  });
-  */
+  function makingcolor() {
+    const color = "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+    return color;
+  }
+
+  function originalColor(li) {
+    li.style.color = "white";
+    li.style.border = "none";
+  }
 };
