f433227b4985f495bc8f4485176180538c7549b4
nav <kil3120@naver.com>
2021-12-13 21:23:46 +0900

    20211213

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   const lis = document.querySelectorAll(".random");
   const mobileChange = document.querySelector(".mobile_change");
+  const scrollCheck = false;
+  const scrollDefaultValue = document.querySelector("html").scrollTop;
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseover", function () {
@@ -10,6 +12,10 @@ window.onload = function () {
       originalColor(lis[i]);
     });
   }
+
+  window.addEventListener("scroll", function () {
+    movedScroll();
+  });
 
   function changeColor(li) {
     const colorValue = makingcolor();
@@ -29,5 +35,19 @@ window.onload = function () {
   function originalColor(li) {
     li.style.color = "white";
     li.style.textShadow = "none";
+  }
+
+  function movedScroll() {
+    const currentScrollTop = document.querySelector("html").scrollTop;
+    const scrolled = 130;
+    const topBtn = document.querySelector(".top_btn");
+
+    if (Math.abs(currentScrollTop - scrollDefaultValue) < scrolled) {
+      topBtn.classList.remove("scrolled");
+    }
+
+    if (currentScrollTop > scrollDefaultValue && currentScrollTop > scrolled) {
+      topBtn.classList.add("scrolled");
+    }
   }
 };
