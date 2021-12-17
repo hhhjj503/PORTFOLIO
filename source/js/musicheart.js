898128d5b0b7e177c13fd0f9726367b78f831042
nav <kil3120@naver.com>
2021-12-17 18:50:23 +0900

    20211217

--- source/js/musicheart.js
+++ source/js/musicheart.js
@@ -17,10 +17,23 @@ window.onload = function () {
   });
   const signupBtn = document.querySelector(".sign_up_btn");
   const subBtn = document.querySelector(".sub_btn");
+  const mobileHMenu = document.querySelector(".mobile_h_menu");
+  const mobileHMenuBtn = document.querySelector(".mobile_h_menu_btn");
+
   signupBtn.addEventListener("click", function () {
     location.href = "../pages/musicheart_join.html";
   });
   subBtn.addEventListener("click", function () {
     location.href = "../pages/musicheart_join.html";
+  });
+
+  mobileHMenuBtn.addEventListener("click", function () {
+    if (mobileHMenuBtn.classList.contains("clicked")) {
+      mobileHMenuBtn.classList.remove("clicked");
+      mobileHMenu.classList.remove("clicked");
+    } else {
+      mobileHMenuBtn.classList.add("clicked");
+      mobileHMenu.classList.add("clicked");
+    }
   });
 };
