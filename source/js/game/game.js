811cc1b666d86b914cbc047597c42aef8af0f586
nav <kil3120@naver.com>
2022-01-28 22:28:51 +0900

    20220128

--- source/js/game/game.js
+++ source/js/game/game.js
@@ -1,9 +1,8 @@
 window.onload = function () {
   const depth1 = document.querySelector("main .contents .menu .depth1");
   const lis = depth1.children;
-  console.log(depth1);
-  console.log(lis);
-  console.log(lis[0].querySelector("a"));
+  const imgScreen = document.querySelector(".image_screen");
+  const items = document.querySelectorAll(".promotion .items .item");
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseenter", function () {
@@ -12,6 +11,12 @@ window.onload = function () {
 
     lis[i].addEventListener("mouseleave", function () {
       removeShow(lis);
+    });
+  }
+
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function () {
+      imgChange(items[i]);
     });
   }
 
@@ -27,5 +32,20 @@ window.onload = function () {
       lis[i].classList.remove("show");
     }
     depth1.classList.remove("show");
+  }
+
+  function imgChange(item) {
+    const number = item.querySelector(".image_number");
+    const imgPath = imgScreen.style.backgroundImage;
+    const numberChk = imgPath.indexOf(number.innerText);
+    if (0 < numberChk) return;
+
+    imgScreen.classList.add("black");
+    const startIndex = imgPath.indexOf("/source");
+    const lastIndex = imgPath.lastIndexOf("/") + 1;
+    const slicedPath = imgPath.slice(startIndex, lastIndex);
+    const path = "../.." + slicedPath + number.innerText + ".png";
+    imgScreen.style.backgroundImage = "url('" + path + "'";
+    imgScreen.classList.remove("black");
   }
 };
