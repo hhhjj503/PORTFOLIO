0c4c64a8b105741d1424cb5c83813c8c3d6984c0
nav <kil3120@naver.com>
2022-02-03 02:42:34 +0900

    20220203

--- source/js/greennature/greennature.js
+++ source/js/greennature/greennature.js
@@ -1,44 +1,36 @@
 window.onload = function () {
-  const swiper = new Swiper(".swiper", {
-    //기본 셋팅
-    //방향 셋팅 vertical 수직, horizontal 수평 설정이 없으면 수평
-    direction: "horizontal",
-    //한번에 보여지는 페이지 숫자
-    slidesPerView: 3,
-    //페이지와 페이지 사이의 간격
-    spaceBetween: 0,
-    //드레그 기능 true 사용가능 false 사용불가
-    debugger: false,
-    //마우스 휠기능 true 사용가능 false 사용불가
-    mousewheel: false,
-    //반복 기능 true 사용가능 false 사용불가
-    loop: false,
-    //선택된 슬라이드를 중심으로 true 사용가능 false 사용불가 djqt
-    centeredSlides: true,
-    // 페이지 전환효과 slidesPerView효과와 같이 사용 불가
-    // effect: 'fade',
+  const prev = document.querySelector(".prev");
+  const next = document.querySelector(".next");
 
-    //자동 스크를링
-    autoplay: {
-      //시간 1000 이 1초
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+  slideWidth = 2100; //350(320 + 30(마진)) * 6
+  next.addEventListener("click", function () {
+    let slider = document.querySelector(".slider");
+    let sliderImg = document.querySelectorAll(".slider .a_item");
 
-    //페이징
-    pagination: {
-      //페이지 기능
-      el: ".swiper-pagination",
-      //클릭 가능여부
-      clickable: true,
-    },
+    let cloneSlide_first = sliderImg[0].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
 
-    //방향표
-    navigation: {
-      //다음페이지 설정
-      nextEl: ".swiper-button-next",
-      //이전페이지 설정
-      prevEl: ".swiper-button-prev",
-    },
+    slider.style.width = +(slideWidth + 350) + "px"; //길이 연장 +350 7개 노드길이로 변경
+    slider.append(cloneSlide_first); //복사한 첫번째 노드를 배열의 맨뒤에 갖다붙임
+    slider.style.left = -350 + "px"; //다음 노드로 이동
+    slider.removeChild(slider.firstElementChild);
+    slider.style.width = -(slideWidth - 350) + "px"; //연장된 길이를 다시 6개 길이로 복구
+  });
+
+  prev.addEventListener("click", function () {
+    let slider = document.querySelector(".slider");
+    let sliderImg = document.querySelectorAll(".slider .a_item");
+    const cnt = sliderImg.length;
+
+    let cloneSlide_last = sliderImg[6].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
+
+    slider.style.width = +(slideWidth + 350) + "px"; //길이 연장 +350 7개 노드길이로 변경
+    slider.insertBefore(cloneSlide_last, slider.firstElementChild); //복사한 첫번째 노드를 배열의 맨뒤에 갖다붙임
+    console.log(slider);
+    console.log(sliderImg);
+
+    slider.style.left = -350 + "px"; //다음 노드로 이동
+    slider.removeChild(slider.lastElementChild);
+
+    slider.style.width = -(slideWidth - 350) + "px"; //연장된 길이를 다시 6개 길이로 복구
   });
 };
