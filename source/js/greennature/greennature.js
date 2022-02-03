97eeeac9c2f1c373b4a2d76229feca282e08c555
nav <kil3120@naver.com>
2022-02-03 14:25:58 +0900

    20220203

--- source/js/greennature/greennature.js
+++ source/js/greennature/greennature.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const prev = document.querySelector(".prev");
   const next = document.querySelector(".next");
+  console.log(prev, next);
 
   slideWidth = 2100; //350(320 + 30(마진)) * 6
   next.addEventListener("click", function () {
@@ -19,9 +20,8 @@ window.onload = function () {
   prev.addEventListener("click", function () {
     let slider = document.querySelector(".slider");
     let sliderImg = document.querySelectorAll(".slider .a_item");
-    const cnt = sliderImg.length;
-
-    let cloneSlide_last = sliderImg[6].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
+    console.log(sliderImg);
+    let cloneSlide_last = sliderImg[5].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
 
     slider.style.width = +(slideWidth + 350) + "px"; //길이 연장 +350 7개 노드길이로 변경
     slider.insertBefore(cloneSlide_last, slider.firstElementChild); //복사한 첫번째 노드를 배열의 맨뒤에 갖다붙임
@@ -29,7 +29,7 @@ window.onload = function () {
     console.log(sliderImg);
 
     slider.style.left = -350 + "px"; //다음 노드로 이동
-    slider.removeChild(slider.lastElementChild);
+    slider.style.transition = `${0.5}s ease-in-out`;
 
     slider.style.width = -(slideWidth - 350) + "px"; //연장된 길이를 다시 6개 길이로 복구
   });
