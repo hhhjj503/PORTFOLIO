5fbe8d7073e6cc791975cd1af6a6c7ff984765c6
nav <kil3120@naver.com>
2022-02-10 03:40:44 +0900

    20220210

--- source/js/greennature/greennature.js
+++ source/js/greennature/greennature.js
@@ -145,32 +145,65 @@ window.onload = function () {
   }
 
   //하단 슬라이더
-  slideWidth = 2100; //350(320 + 30(마진)) * 6
   next.addEventListener("click", function () {
-    let slider = document.querySelector(".slider");
-    let sliderImg = document.querySelectorAll(".slider .a_item");
+    let slider = document.querySelector(".slider"); //ul
+    let sliderImg = document.querySelectorAll(".slider .a_item"); //ul li
     let cloneSlide_first = sliderImg[0].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
 
-    slider.style.width = +(slideWidth + 350) + "px"; //길이 연장 +350 7개 노드길이로 변경
-    slider.append(cloneSlide_first); //복사한 첫번째 노드를 배열의 맨뒤에 갖다붙임
-    slider.style.left = -350 + "px"; //다음 노드로 이동
+    let slideWidth = 2100; //350(320 + 30(마진)) * 6
+    const leftWidth = -30 + "px";
+    slideWidth = slideWidth + 350; // 복사한 첫번째 노드를 붙이기 위한 너비 증가 2400
+    setTimeout(() => {
+      slider.style.width = slideWidth + "px"; //1400px
+    }, 100);
 
-    slider.removeChild(slider.firstElementChild);
-    slider.style.width = -(slideWidth - 350) + "px"; //연장된 길이를 다시 6개 길이로 복구
+    setTimeout(() => {
+      slider.appendChild(cloneSlide_first);
+    }, 150);
+
+    setTimeout(() => {
+      slideWidth = slideWidth - 350; // 다시 원래 너비로 2150
+      slider.style.width = slideWidth + "px";
+    }, 200);
+
+    setTimeout(() => {
+      slider.style.left = leftWidth;
+    }, 300);
+
+    setTimeout(() => {
+      slider.style.left = 0;
+      slider.removeChild(slider.firstElementChild);
+    }, 400);
   });
 
   prev.addEventListener("click", function () {
-    let slider = document.querySelector(".slider");
-    let sliderImg = document.querySelectorAll(".slider .a_item");
-    let cloneSlide_last = sliderImg[5].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
+    let slider = document.querySelector(".slider"); //ul
+    let sliderImg = document.querySelectorAll(".slider .a_item"); //ul li
+    let cloneSlide_last = sliderImg[sliderImg.length - 1].cloneNode(true); //next가 넘어가면 배열의 첫번째 노드를 복사
 
-    slider.style.transition = `${0.5}s ease-out`;
-    slider.style.width = +(slideWidth + 350) + "px"; //길이 연장 +350 7개 노드길이로 변경
-    slider.insertBefore(cloneSlide_last, slider.firstElementChild); //복사한 첫번째 노드를 배열의 맨뒤에 갖다붙임
+    let slideWidth = 2100; //350(320 + 30(마진)) * 6
+    const leftWidth = -30 + "px";
+    slideWidth = slideWidth + 350; // 복사한 첫번째 노드를 붙이기 위한 너비 증가 2400
+    setTimeout(() => {
+      slider.style.width = slideWidth + "px"; //1400px
+    }, 100);
 
-    slider.style.right = -350 + "px"; //다음 노드로 이동
-    slider.removeChild(slider.lastElementChild);
+    setTimeout(() => {
+      slider.insertBefore(cloneSlide_last, slider.firstElementChild);
+    }, 150);
 
-    slider.style.width = -(slideWidth - 350) + "px"; //연장된 길이를 다시 6개 길이로 복구
+    setTimeout(() => {
+      slideWidth = slideWidth - 350; // 다시 원래 너비로 2150
+      slider.style.width = slideWidth + "px";
+    }, 200);
+
+    setTimeout(() => {
+      slider.style.left = leftWidth;
+    }, 300);
+
+    setTimeout(() => {
+      slider.removeChild(slider.lastElementChild);
+      slider.style.left = 0;
+    }, 400);
   });
 };
