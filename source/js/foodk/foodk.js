dd3513ab64efa41368adc44e051c08db48ef2c29
nav <kil3120@naver.com>
2022-03-22 04:06:36 +0900

    20220322

--- source/js/foodk/foodk.js
+++ source/js/foodk/foodk.js
@@ -6,6 +6,8 @@ window.onload = function () {
   const indexs = document.querySelectorAll(".path_wrapper .index");
   const paths = document.querySelectorAll(".path_wrapper .path");
   let currentIndex = 0;
+  let nextIndex = currentIndex + 1;
+  let clickedIndex = 0;
   const wrapperCnt = 5;
 
   for (let i = 0; i < pathWrappers.length; i++) {
@@ -17,29 +19,42 @@ window.onload = function () {
   pathWrappers[0].classList.add("clicked");
 
   setInterval(() => {
-    currentIndex += 1;
-    if (currentIndex >= wrapperCnt) {
-      currentIndex = 0;
+    nextIndex = currentIndex + 1;
+    console.log(currentIndex, nextIndex, clickedIndex);
+    if (nextIndex >= wrapperCnt) {
+      nextIndex = 0;
+      changeImg(
+        pathWrappers,
+        pathWrappers[nextIndex],
+        indexs[nextIndex],
+        paths[nextIndex]
+      );
+      return;
     }
     changeImg(
       pathWrappers,
-      pathWrappers[currentIndex],
-      indexs[currentIndex],
-      paths[currentIndex]
+      pathWrappers[nextIndex],
+      indexs[nextIndex],
+      paths[nextIndex]
     );
-  }, 2000);
+  }, 3000);
 
   // 메인배너 이미지 변경
   function changeImg(pathWrappers, pathWrapper, index, path) {
-    const intIndex = parseInt(index.innerHTML);
-    if (currentIndex == intIndex) return;
+    clickedIndex = parseInt(index.innerHTML);
+    currentIndex = clickedIndex;
     for (let i = 0; i < pathWrappers.length; i++) {
       pathWrappers[i].classList.remove("clicked");
     }
-    screen.style.backgroundImage = "url('" + path.innerHTML + "')";
-    pathWrapper.classList.add("clicked");
-    currentIndex = intIndex;
-    alert(currentIndex + typeof currentIndex);
-    alert("???");
+    if (currentIndex === clickedIndex) {
+      nextIndex = currentIndex;
+      screen.style.backgroundImage = "url('" + path.innerHTML + "')";
+      pathWrapper.classList.add("clicked");
+    } else {
+      screen.style.backgroundImage = "url('" + path.innerHTML + "')";
+      pathWrapper.classList.add("clicked");
+      currentIndex = nextIndex;
+    }
+    console.log(currentIndex, nextIndex, clickedIndex);
   }
 };
