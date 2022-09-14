4de7b4e83d704614cadddc2b7a779dea751bd49e
nav <kil3120@naver.com>
2022-09-14 21:24:55 +0900

    20220914

---
+++ source/js/hoveryourcolor/index.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function () {
+  const links = document.querySelectorAll(".container p > a");
+  let bg = document.querySelector(".bg");
+  const title = document.querySelector("h2");
+  const divLeft = this.document.querySelector("div.left");
+  const divRight = this.document.querySelector("div.right");
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseenter", function (e) {
+      changeBG(e, links);
+    });
+    links[i].addEventListener("mouseleave", function (e) {
+      leaveBG(links);
+    });
+  }
+
+  function changeBG(e, links) {
+    const path = e.target.getAttribute("data-path");
+    divLeft.classList.add("open");
+    divRight.classList.add("open");
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.add("hidden");
+    }
+    title.style.opacity = 0;
+    e.target.classList.remove("hidden");
+    bg.style.backgroundImage = `url(${path})`;
+    bg.style.opacity = 1;
+  }
+
+  function leaveBG(links) {
+    bg.style.opacity = 0;
+    divLeft.classList.remove("open");
+    divRight.classList.remove("open");
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.remove("hidden");
+    }
+    title.style.opacity = 1;
+  }
+});
