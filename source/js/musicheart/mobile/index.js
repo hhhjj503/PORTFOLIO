b0ee450fbcb16f10438217b5e7fe09e5246fd7b1
hhhjj503 <kil3120@naver.com>
2022-10-03 03:30:04 +0900

    20221003

--- source/js/musicheart/mobile/index.js
+++ source/js/musicheart/mobile/index.js
@@ -4,22 +4,6 @@ window.onload = function () {
     window.location.href = "../index.html";
   }
 
-  const swiper = new Swiper(".swiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
   const signup = document.querySelector(".sign_up");
   const login = document.querySelector(".login");
   const subBtn = document.querySelector(".sub_btn");
