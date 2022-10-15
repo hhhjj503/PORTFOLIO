7592184f544049b8ed593d0a4a3a29a7f3db65d8
hhhjj503 <kil3120@naver.com>
2022-10-16 01:17:00 +0900

    20221016

---
+++ source/js/amusementpark/attraction/attraction.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", () => {
+  const html = document.querySelector("html");
+  const topNav = document.querySelector(".top-nav");
+  const topNavMobile = document.querySelector(".top-nav-mobile");
+  const subMenu = document.querySelector(".top-nav-mobile .sub-menu");
+  const goup = document.querySelector(".top-nav-mobile .goup");
+  const hidings = document.querySelectorAll("a.hiding + *");
+
+  for (let i = 0; i < hidings.length; i++) {
+    hidings[i].dataset.offtop = hidings[i].getBoundingClientRect().top;
+  }
+
+  window.addEventListener("scroll", () => {
+    const currentTop = html.scrollTop;
+    for (let i = 0; i < hidings.length; i++) {
+      if (currentTop >= hidings[i].dataset.offtop) {
+        showElements(hidings[i]);
+      }
+    }
+  });
+
+  function showElements(element) {
+    element.style.top = "0px";
+    element.style.opacity = "1";
+  }
+
+  window.addEventListener("scroll", () => {
+    if (html.scrollTop > 0) {
+      topNav.style.opacity = "0";
+      topNavMobile.style.opacity = "0";
+    } else {
+      topNav.style.opacity = "1";
+      topNavMobile.style.opacity = "1";
+    }
+  });
+
+  topNavMobile.addEventListener("click", () => {
+    subMenu.classList.toggle("opened");
+  });
+
+  //click 이벤트 작동하지 않음
+  goup.addEventListener("mouseenter", () => {
+    subMenu.classList.toggle("opened");
+  });
+});
