72a890665dc5c6bee9a6caca1d9b5638891eef29
hhhjj503 <kil3120@naver.com>
2023-04-04 19:59:08 +0900

    20230404

--- source/js/amusementpark/index.js
+++ source/js/amusementpark/index.js
@@ -4,18 +4,18 @@ window.addEventListener("load", () => {
   const descs = document.querySelectorAll(".desc");
   const backgrounds = document.querySelectorAll(".background");
 
-  for (let i = 0; i < types.length; i++) {
+  /* for (let i = 0; i < types.length; i++) {
     types[i].addEventListener("mouseover", () => {
       changeColor(descs[i]);
     });
+
+     function changeColor(color) {
+    footer.style.backgroundColor = color.dataset.color;
   }
+  } */
 
   for (const background of backgrounds) {
     const img = new Image();
     img.src = background.dataset.path;
-  }
-
-  function changeColor(color) {
-    footer.style.backgroundColor = color.dataset.color;
   }
 });
