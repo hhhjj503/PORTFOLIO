e77e8a0db16fa584e138f44b3f8efe74cf610591
hhhjj503 <kil3120@naver.com>
2023-09-30 17:08:35 +0900

    20230930

--- source/js/index.js
+++ source/js/index.js
@@ -3,17 +3,16 @@ window.addEventListener("load", () => {
   const header = document.querySelector("header");
   const headerAs = document.querySelectorAll("header nav a"); //헤더클릭시 스크롤이동
   const graph = document.querySelector(".graph"); //그래프클래스제어
-  const items = document.querySelectorAll(".item"); //스크롤이동할 타이틀컴포넌트
-  const wrappers = document.querySelectorAll(".wrapper"); //스크롤이동할 타이틀컴포넌트
+  const scrollElements = document.querySelectorAll(".scroll-element"); //스크롤이동할 엘리먼트 쿼리용 클래스
   const decoLine = document.querySelector(".deco-line");
-  const vertexs = document.querySelectorAll(".vertex");
-  const skills = document.querySelector(".skills");
-  const portfolio = document.querySelector(".portfolio");
-  const goup = document.querySelector(".goup");
+  const vertexLis = document.querySelectorAll(".vertex-li");
+  const skills = document.querySelector("#skills");
+  const portfolio = document.querySelector("#portfolio");
+  const gotop = document.querySelector(".gotop");
 
-  let itemsOffsetTops = [];
-  let vertexsOffsetLefts = [];
-  let vertexsIndex = 0;
+  let scrollElementsOffsetTops = [];
+  let vertexLisOffsetLefts = [];
+  let vertexLisIndex = 0;
   //배열에 스크롤 이벤트 트리거가 되는 엘리먼트의 top 값을 저장
   saveOffsetTop();
   saveOffsetLeft();
@@ -23,7 +22,7 @@ window.addEventListener("load", () => {
     headerAs[i].addEventListener("click", () => {
       //e.preventDefault();
       window.scrollTo({
-        top: itemsOffsetTops[i],
+        top: scrollElementsOffsetTops[i],
         left: 0,
       });
     });
@@ -32,7 +31,7 @@ window.addEventListener("load", () => {
       if (e.key === "Enter") {
         e.preventDefault();
         window.scrollTo({
-          top: itemsOffsetTops[i],
+          top: scrollElementsOffsetTops[i],
           left: 0,
           behavior: "smooth",
         });
@@ -43,27 +42,27 @@ window.addEventListener("load", () => {
   }
 
   //vertex에 기본적인 이벤트 추가
-  for (let i = 0; i < vertexs.length; i++) {
-    vertexs[i].addEventListener("mouseenter", () => {
-      vertexs.forEach((vertex) => vertex.classList.remove("active"));
-      vertexs[i].classList.add("active");
-      vertexsIndex = i;
-      decoLine.style.width = vertexsOffsetLefts[i] + "px";
+  for (let i = 0; i < vertexLis.length; i++) {
+    vertexLis[i].addEventListener("mouseenter", () => {
+      vertexLis.forEach((vertex) => vertex.classList.remove("active"));
+      vertexLis[i].classList.add("active");
+      vertexLisIndex = i;
+      decoLine.style.width = vertexLisOffsetLefts[i] + "px";
     });
-    vertexs[i].addEventListener("click", () => {
-      vertexs.forEach((vertex) => vertex.classList.remove("active"));
-      vertexs[i].classList.add("active");
-      vertexsIndex = i;
-      decoLine.style.width = vertexsOffsetLefts[i] + "px";
+    vertexLis[i].addEventListener("click", () => {
+      vertexLis.forEach((vertex) => vertex.classList.remove("active"));
+      vertexLis[i].classList.add("active");
+      vertexLisIndex = i;
+      decoLine.style.width = vertexLisOffsetLefts[i] + "px";
     });
-    vertexs[i].addEventListener("mouseleave", () => {
-      vertexs[i].classList.remove("active");
+    vertexLis[i].addEventListener("mouseleave", () => {
+      vertexLis[i].classList.remove("active");
     });
-    vertexs[i].addEventListener("keydown", (e) => {
+    vertexLis[i].addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
-        vertexs.forEach((vertex) => vertex.classList.remove("active"));
-        vertexs[i].classList.add("active");
-        decoLine.style.width = vertexs[i].offsetLeft + "px";
+        vertexLis.forEach((vertex) => vertex.classList.remove("active"));
+        vertexLis[i].classList.add("active");
+        decoLine.style.width = vertexLis[i].offsetLeft + "px";
       }
     });
   }
@@ -73,11 +72,11 @@ window.addEventListener("load", () => {
   }, 1500);
   setTimeout(() => {
     const event = new Event("mouseenter");
-    vertexs[0].dispatchEvent(event);
+    vertexLis[0].dispatchEvent(event);
   }, 2000);
 
   //위로가기버튼
-  goup.addEventListener("click", (e) => {
+  gotop.addEventListener("click", (e) => {
     e.preventDefault();
     window.scrollTo({
       top: 0,
@@ -92,8 +91,8 @@ window.addEventListener("load", () => {
   window.addEventListener(
     "scroll",
     throttle(() => {
-      const skillsTrigger = itemsOffsetTops[1] * 0.7;
-      const portfolioTrigger = itemsOffsetTops[2] * 0.9;
+      const skillsTrigger = scrollElementsOffsetTops[1] * 0.7;
+      const portfolioTrigger = scrollElementsOffsetTops[2] * 0.9;
 
       if (html.scrollTop > skillsTrigger) skills.classList.add("active");
       else skills.classList.remove("active");
@@ -102,8 +101,8 @@ window.addEventListener("load", () => {
       else portfolio.classList.remove("active");
 
       if (html.scrollTop > html.scrollHeight * 0.5)
-        goup.classList.add("active");
-      else goup.classList.remove("active");
+        gotop.classList.add("active");
+      else gotop.classList.remove("active");
     }),
     { passive: true }
   );
@@ -112,15 +111,15 @@ window.addEventListener("load", () => {
   window.addEventListener("resize", () => {
     saveOffsetLeft();
     saveOffsetTop();
-    decoLine.style.width = vertexsOffsetLefts[vertexsIndex] + "px";
-    for (let i = 0; i < vertexs.length; i++) {
-      vertexs[i].addEventListener("mouseenter", () => {
-        vertexs.forEach((vertex) => vertex.classList.remove("active"));
-        vertexs[i].classList.add("active");
-        decoLine.style.width = vertexs[i].offsetLeft + "px";
+    decoLine.style.width = vertexLisOffsetLefts[vertexLisIndex] + "px";
+    for (let i = 0; i < vertexLis.length; i++) {
+      vertexLis[i].addEventListener("mouseenter", () => {
+        vertexLis.forEach((vertex) => vertex.classList.remove("active"));
+        vertexLis[i].classList.add("active");
+        decoLine.style.width = vertexLis[i].offsetLeft + "px";
       });
-      vertexs[i].addEventListener("mouseleave", () => {
-        vertexs[i].classList.remove("active");
+      vertexLis[i].addEventListener("mouseleave", () => {
+        vertexLis[i].classList.remove("active");
       });
     }
   });
@@ -139,21 +138,21 @@ window.addEventListener("load", () => {
   } //throttle
 
   function saveOffsetTop() {
-    itemsOffsetTops = [];
+    scrollElementsOffsetTops = [];
     for (let i = 0; i < headerAs.length; i++) {
-      itemsOffsetTops.push(
-        items[i].getBoundingClientRect().top +
+      scrollElementsOffsetTops.push(
+        scrollElements[i].getBoundingClientRect().top +
           window.pageYOffset -
           header.offsetHeight
       );
     }
-    itemsOffsetTops[headerAs.length - 1] = html.scrollHeight;
+    scrollElementsOffsetTops[headerAs.length - 1] = html.scrollHeight;
   } //창 사이즈 변경시 top값 다시저장
 
   function saveOffsetLeft() {
-    vertexsOffsetLefts = [];
-    for (let i = 0; i < vertexs.length; i++) {
-      vertexsOffsetLefts.push(vertexs[i].offsetLeft);
+    vertexLisOffsetLefts = [];
+    for (let i = 0; i < vertexLis.length; i++) {
+      vertexLisOffsetLefts.push(vertexLis[i].offsetLeft);
     }
   } //창 사이즈 변경시 vertex left값 다시저장
 });
