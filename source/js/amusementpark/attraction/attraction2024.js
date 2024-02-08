ad9c1f3d11af406589a2af3a23c6ba8b816e358a
hhhjj503 <kil3120@naver.com>
2024-02-08 17:18:46 +0900

    20240208

--- source/js/amusementpark/attraction/attraction2024.js
+++ source/js/amusementpark/attraction/attraction2024.js
@@ -1,1 +1,27 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  //scroll-up
+  const scrollUpLink = document.querySelector(".up-link");
+  const html = document.querySelector("html");
+
+  //모바일 사이드용 메뉴 open close
+  const mobileHeaderMenuBtn = document.querySelector(".mobile-side-menu-btn");
+  const mobileHeaderMenuCloseBtn = document.querySelector(
+    ".mobile-side-menu-close-btn"
+  );
+  const mobileSideMenu = document.querySelector(".mobile-side-menu");
+
+  scrollUpLink.addEventListener("click", function () {
+    html.scrollTop = 0;
+  });
+  window.addEventListener("scroll", () => {
+    if (html.scrollTop > 1) scrollUpLink.classList.add("active");
+    else scrollUpLink.classList.remove("active");
+  });
+
+  mobileHeaderMenuBtn.addEventListener("click", function () {
+    mobileSideMenu.classList.add("active");
+  });
+  mobileHeaderMenuCloseBtn.addEventListener("click", function () {
+    mobileSideMenu.classList.remove("active");
+  });
+});
